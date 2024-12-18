48bc88e87f3143ea0b85f92245e97711c2d6800e
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-12-18 09:31:05 +0100

    Connection done, gitignore updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.example.db.MySQLConnection.getConnection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        //Testing connection
+        String sql = "SELECT * FROM COUNTRY";
+        try(
+                Connection connection = getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(sql);
+        ){
+            while(resultSet.next()){
+                System.out.println("Name: " + resultSet.getString("Name"));
+                System.out.println("Continent: " + resultSet.getString("Continent"));
+                System.out.println("Population: " + resultSet.getString("Population"));
+            }
+        }
+        catch(SQLException e){
+            e.getStackTrace();
+        }
     }
 }
